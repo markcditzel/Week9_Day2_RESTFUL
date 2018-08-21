dcb9496114d160bc4444494698dd947d3aa79b65
Mark Ditzel <mark.ditzel@ed.ac.uk>
2018-08-21 21:27:46 +0100

    cant get the GET SHOW to work

--- src/main/java/controllers/ManagersController.java
+++ src/main/java/controllers/ManagersController.java
@@ -1,33 +1,67 @@
 package controllers;
 
 import db.DBHelper;
+import models.Department;
 import models.Manager;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class ManagersController {
-
-    // Constructor
-    public ManagersController(){
-        setupEndpoint();
+    public ManagersController() {
+        this.setupEndpoints();
     }
 
-    private void setupEndpoint(){
-        get("/managers", (req, res) ->{
+    private void setupEndpoints() {
+
+
+        get("/managers", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("template", "/templates/managers/index.vtl");
             List<Manager> managers = DBHelper.getAll(Manager.class);
+            model.put("template", "templates/managers/index.vtl");
             model.put("managers", managers);
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+
+        get ("/managers/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Department> departments = DBHelper.getAll(Department.class);
+            model.put("departments", departments);
+            model.put("template", "templates/managers/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post ("/managers", (req, res) -> {
+            int departmentId = Integer.parseInt(req.queryParams("department"));
+            Department department = DBHelper.find(departmentId, Department.class);
+            String firstName = req.queryParams("firstName");
+            String lastName = req.queryParams("lastName");
+            int salary = Integer.parseInt(req.queryParams("salary"));
+            int budget = Integer.parseInt(req.queryParams("budget"));
+            Manager manager = new Manager(firstName, lastName, salary, department, budget);
+            DBHelper.save(manager);
+            res.redirect("/managers");
+            return null;
+        });
+
+        get("/managers/:id/show", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            int id = Integer.parseInt(req.params(":id"));
+            Manager manager = DBHelper.find(id, Manager.class);
+            model.put("template", "templates/managers/show.vtl");
+            model.put("manager", manager);
+            return new ModelAndView(model, "templates/show.vtl");
+        }, new VelocityTemplateEngine());
+
+
+
+
     }
-
-
 }
+
